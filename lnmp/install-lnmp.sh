69e990c1dd7a184c82b2fba72d375f5f0f2fca8b
mjy191 <mjy191@163.com>
2022-10-05 13:13:48 +0800

    优化mysql拷贝data的目录逻辑

--- lnmp/install-lnmp.sh
+++ lnmp/install-lnmp.sh
@@ -8,7 +8,6 @@ if [ ! $conf_dir ];then
 fi
 
 echo "start nginx conf"
-docker pull nginx:latest
 docker run -it -d --name nginx-test nginx:latest
 mkdir -p $conf_dir/nginx/conf
 chmod -R 777 $conf_dir/nginx/conf
@@ -24,7 +23,6 @@ chmod -R 777 $conf_dir/nginx/log
 echo "end nginx conf"
 
 echo "start php-fpm"
-docker pull php:7.4-fpm
 docker run -it -d --name php-test php:7.4-fpm
 mkdir -p $conf_dir/php-fpm7.4/conf
 docker cp php-test:/usr/local/etc/. $conf_dir/php-fpm7.4/conf/
@@ -42,15 +40,16 @@ chmod -R 777 $confi_dir/php-fpm7.4/logs
 echo "end php-fpm"
 
 echo "start mysql conf"
-docker pull mysql:5.7.39
-docker run -it -d --name mysql-test mysql:5.7.39
-mkdir -p $conf_dir/mysql/etc
-docker cp mysql-test:/etc/my.cnf $conf_dir/mysql/etc/
+docker run -it -d -e MYSQL_ROOT_PASSWORD=123456 --name mysql-test mysql:5.7.39
+until docker logs mysql-test | grep 'ready for connections'
+do
+	mkdir -p $conf_dir/mysql/etc
+	mkdir -p $conf_dir/mysql/data
+	chmod -R 777 $conf_dir/mysql/data
+	docker cp mysql-test:/etc/my.cnf $conf_dir/mysql/etc/
+	docker cp mysql-test:/var/lib/mysql/. $conf_dir/mysql/data/
+done
 docker rm -f mysql-test
-cp -f $conf_dir/mysql/etc/my.cnf $conf_dir/mysql/etc/my.bak
-cp -f mysql/my.cnf $conf_dir/mysql/etc/
-mkdir -p $conf_dir/mysql/data
-chmod -R 777 $conf_dir/mysql/data
 echo "end mysql conf"
 
 echo "start redis conf"
